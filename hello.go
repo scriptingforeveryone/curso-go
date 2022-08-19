fbe6f0d3aa9c21e30851a78a22eafe316a3c8311
andremachado8 <andreeeemachado@gmail.com>
2022-08-19 04:12:59 -0300

    Ponteiro com Golang 1

--- hello.go
+++ hello.go
@@ -2,36 +2,7 @@ package main
 
 import "fmt"
 
-type Carro struct {
-	Nome string
-}
-
-func (c Carro) andar() {
-	fmt.Println(c.Nome, "andou")
-}
-
 func main() {
-
-	carro := Carro{
-		Nome: "BMW",
-	}
-
-	carro.andar()
-
-	resultado := func(x ...int) func() int {
-		res := 0
-
-		for _, v := range x {
-			res += v
-		}
-
-		return func() int {
-			return res * res
-		}
-
-	}
-	// resultado := somaTudo(10, 20, 40)
-
-	fmt.Println(resultado(54, 54, 54, 54)())
-
+	a := 10
+	fmt.Println(&a)
 }
